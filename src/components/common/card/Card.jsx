c1fd167d132e368557931550e6ea085c2f70e400
Josue Inzunza <josueinzunzaprojects@gmail.com>
2024-10-27 20:35:58 -0700

    Enrutado2

--- src/components/common/card/Card.jsx
+++ src/components/common/card/Card.jsx
@@ -1,9 +1,15 @@
-export const Card = ({ title, price, stock }) => {
+import { Link } from "react-router-dom";
+
+export const Card = ({ title, price, stock, image, id }) => {
   return (
     <div style={{ border: "2px solid black", width: "200px", height: "200px" }}>
+      <img src={image} alt="" style={{ width: "100px" }} />
       <h2>Titulo: {title}</h2>
       <h3>Precio: {price}</h3>
       <h3>Stock: {stock}</h3>
+      <Link to={`/itemDetail/${id}`}>
+        <button>Ver detalle</button>
+      </Link>
     </div>
   );
 };
